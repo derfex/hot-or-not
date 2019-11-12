193cd3dedce63eee0348f2674aa7e356b8c931ec
derfex <dm1tr0@mail.ru>
2019-11-13 00:04:33 +0300

    [Decorations] AppBar. Create component

---
+++ src/components/HighLevel/AppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar
+    app
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-icon large color="yellow darken-2">mdi-call-split</v-icon>
+      <v-toolbar-title>HOT or NOT?</v-toolbar-title>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/derfex/hot-or-not"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Source</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+};
+</script>
