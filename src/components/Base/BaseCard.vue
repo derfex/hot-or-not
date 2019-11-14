cdbc1a745fb11eb033af0f42ce03159e07380cde
derfex <dm1tr0@mail.ru>
2019-11-14 03:08:50 +0300

    BaseCard. Create component

---
+++ src/components/Base/BaseCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card
+    outlined
+    width="400px"
+  >
+    <div class="base-card__image-box">
+      <v-img
+        :src="data.path"
+        class="ma-auto"
+        width="200px"
+        height="200px"
+      />
+    </div>
+    <v-card-title>{{ data.title }}</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard',
+
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.base-card__image-box {
+  background-color: #222;
+}
+</style>
